4e4c01a35cdd044f9f58a2009b6e7c32ec81542d
takapg <75102730+takapg@users.noreply.github.com>
2026-06-01 12:21:03 +0900

    Add RubyMysqlTui module with logger setup

---
+++ lib/ruby_mysql_tui.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'logger'
+
+# TODO 適切なコメントに変更
+module RubyMysqlTui
+  # ロガーの設定
+  @logger_mutex = Mutex.new
+
+  def self.logger
+    return @logger if @logger
+
+    @logger_mutex.synchronize do
+      @logger ||= Logger.new($stdout).tap do |log|
+        level_name = ENV.fetch('LOG_LEVEL', 'DEBUG').upcase
+        log.level = Logger.const_defined?(level_name) ? Logger.const_get(level_name) : Logger::DEBUG
+        log.formatter = proc do |severity, datetime, _progname, msg|
+          "#{datetime.strftime('%Y-%m-%d %H:%M:%S')} [#{severity}] #{msg}\n"
+        end
+      end
+    end
+  end
+
+  def self.start
+    logger.info 'Starting RubyMysqlTui...'
+    # TODO 実装
+  end
+end
